79dcc2d523c91f735f3da52f829810d5b18ba146
ChenShiWen <cswzmg@live.com>
2016-05-13 14:32:45 +0800

    Initial Commit

---
+++ 3DTouchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  3DTouchDemo
+//
+//  Created by 小城生活 on 16/5/13.
+//  Copyright © 2016年 小城生活. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 3DTouchDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  3DTouchDemo
+//
+//  Created by 小城生活 on 16/5/13.
+//  Copyright © 2016年 小城生活. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
